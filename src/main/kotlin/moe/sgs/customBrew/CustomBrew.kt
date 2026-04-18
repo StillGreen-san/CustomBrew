0c831bdeb652258b0b82df613088b7f5f6f2260e
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2026-04-18 13:11:10 +0200

    init

---
+++ src/main/kotlin/moe/sgs/customBrew/CustomBrew.kt
@@ -0,0 +1,14 @@
+package moe.sgs.customBrew
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CustomBrew : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
