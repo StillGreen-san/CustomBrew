85531299eee1426670d4d2c560cfeba3d78d1568
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2026-04-25 20:32:02 +0200

    refactor addMix

--- src/main/kotlin/moe/sgs/customBrew/CustomBrew.kt
+++ src/main/kotlin/moe/sgs/customBrew/CustomBrew.kt
@@ -14,79 +14,65 @@ import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 import org.bukkit.potion.PotionType
+import kotlin.random.Random
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
 class CustomBrew : JavaPlugin() {
 
-    private val keyHaste = NamespacedKey(this, "potion_of_haste")
-    private val keyLongHaste = NamespacedKey(this, "potion_of_long_haste")
-    private val keyStrongHaste = NamespacedKey(this, "potion_of_strong_haste")
+    private val durationBase = 3.minutes.inWholeTicks.toInt()
+    private val durationLong = 8.minutes.inWholeTicks.toInt()
+    private val durationStrong = 1.5.minutes.inWholeTicks.toInt()
+
+    private val mixKeys = ArrayList<NamespacedKey>(9)
 
     override fun onEnable() {
-        val hastePotion = ItemStack(Material.POTION)
-        hastePotion.editMeta {
-            it.customName(
-                Component.translatable(
-                    "potion.withAmplifier",
-                    Style.style().decoration(TextDecoration.ITALIC, false).build(),
-                    Component.translatable("effect.minecraft.haste"),
-                    Component.translatable("item.minecraft.potion")
-                )
-            )
-            (it as PotionMeta).addCustomEffect(
-                PotionEffect(PotionEffectType.HASTE, 3.minutes.inWholeTicks.toInt(), 0),
-                true
-            )
-        }
         val thickPotion = ItemStack(Material.POTION)
         thickPotion.editMeta {
             (it as PotionMeta).basePotionType = PotionType.THICK
         }
-        Bukkit.getPotionBrewer().addPotionMix(
-            PotionMix(
-                keyHaste,
-                hastePotion,
-                RecipeChoice.ExactChoice(thickPotion),
-                RecipeChoice.ExactChoice(ItemStack(Material.HONEYCOMB_BLOCK)),
-            )
-        )
-        val longHastePotion = hastePotion.clone()
-        longHastePotion.editMeta {
-            (it as PotionMeta).addCustomEffect(
-                PotionEffect(PotionEffectType.HASTE, 8.minutes.inWholeTicks.toInt(), 0),
-                true
-            )
-        }
-        Bukkit.getPotionBrewer().addPotionMix(
-            PotionMix(
-                keyLongHaste,
-                longHastePotion,
-                RecipeChoice.ExactChoice(hastePotion),
-                RecipeChoice.ExactChoice(ItemStack(Material.REDSTONE)),
-            )
-        )
-        val strongHastePotion = hastePotion.clone()
-        strongHastePotion.editMeta {
-            (it as PotionMeta).addCustomEffect(
-                PotionEffect(PotionEffectType.HASTE, 1.5.minutes.inWholeTicks.toInt(), 1),
-                true
-            )
-        }
-        Bukkit.getPotionBrewer().addPotionMix(
-            PotionMix(
-                keyStrongHaste,
-                strongHastePotion,
-                RecipeChoice.ExactChoice(hastePotion),
-                RecipeChoice.ExactChoice(ItemStack(Material.GLOWSTONE_DUST)),
-            )
-        )
+        val hastePotion = addMix(thickPotion, durationBase, 0, Material.HONEYCOMB_BLOCK, "potion")
+        /*val longHastePotion =*/ addMix(hastePotion, durationLong, 0, Material.REDSTONE)
+        /*val strongHastePotion =*/ addMix(hastePotion, durationStrong, 1, Material.GLOWSTONE_DUST)
     }
 
     override fun onDisable() {
-        Bukkit.getPotionBrewer().removePotionMix(keyHaste)
-        Bukkit.getPotionBrewer().removePotionMix(keyLongHaste)
-        Bukkit.getPotionBrewer().removePotionMix(keyStrongHaste)
+        mixKeys.forEach {
+            Bukkit.getPotionBrewer().removePotionMix(it)
+        }
+    }
+
+    private fun addMix(
+        basePotion: ItemStack, duration: Int, amplifier: Int, ingredient: Material, nameKey: String? = null
+    ): ItemStack {
+        val resultPotion = basePotion.clone()
+        resultPotion.editMeta {
+            if (nameKey != null) {
+                it.customName(
+                    Component.translatable(
+                        "potion.withAmplifier",
+                        Style.style().decoration(TextDecoration.ITALIC, false).build(),
+                        Component.translatable("effect.minecraft.haste"),
+                        Component.translatable("item.minecraft.$nameKey")
+                    )
+                )
+            }
+            (it as PotionMeta).addCustomEffect(
+                PotionEffect(PotionEffectType.HASTE, duration, amplifier),
+                true
+            )
+        }
+        val mixKey = NamespacedKey(this, Random.nextLong().toHexString())
+        Bukkit.getPotionBrewer().addPotionMix(
+            PotionMix(
+                mixKey,
+                resultPotion,
+                RecipeChoice.ExactChoice(basePotion),
+                RecipeChoice.ExactChoice(ItemStack(ingredient)),
+            )
+        )
+        mixKeys.add(mixKey)
+        return resultPotion
     }
 }
 
